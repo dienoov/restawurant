9f1e908f8fc6ceca1203b39b1d5e5d961d670277
dienoov <alvinodien@yahoo.co.id>
2025-04-10 08:55:25 +0700

    fix: correct restaurant detail fields

--- lib/models/restaurant_detail.dart
+++ lib/models/restaurant_detail.dart
@@ -7,25 +7,25 @@ RestaurantDetail restaurantDetailFromJson(String str) => RestaurantDetail.fromJs
 String restaurantDetailToJson(RestaurantDetail data) => json.encode(data.toJson());
 
 class RestaurantDetail {
-  final bool? error;
-  final String? message;
-  final Restaurant? restaurant;
+  final bool error;
+  final String message;
+  final Restaurant restaurant;
 
   RestaurantDetail({
-    this.error,
-    this.message,
-    this.restaurant,
+    required this.error,
+    required this.message,
+    required this.restaurant,
   });
 
   factory RestaurantDetail.fromJson(Map<String, dynamic> json) => RestaurantDetail(
     error: json["error"],
     message: json["message"],
-    restaurant: json["restaurant"] == null ? null : Restaurant.fromJson(json["restaurant"]),
+    restaurant: Restaurant.fromJson(json["restaurant"]),
   );
 
   Map<String, dynamic> toJson() => {
     "error": error,
     "message": message,
-    "restaurant": restaurant?.toJson(),
+    "restaurant": restaurant.toJson(),
   };
 }
