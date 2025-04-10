19ca84f326d21d9a64a59fe1b1a02710bc61de83
dienoov <alvinodien@yahoo.co.id>
2025-04-10 08:56:30 +0700

    chore: add api for fetching restaurant detail

--- lib/apis/restaurants.dart
+++ lib/apis/restaurants.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:restawurant/models/restaurant_list.dart';
 
+import '../models/restaurant_detail.dart';
+
 class RestaurantsApi {
   static const String _baseUrl = "https://restaurant-api.dicoding.dev";
 
@@ -13,6 +15,16 @@ class RestaurantsApi {
       return RestaurantList.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load restaurant list');
+    }
+  }
+
+  Future<RestaurantDetail> detail(String id) async {
+    final response = await http.get(Uri.parse("$_baseUrl/detail/$id"));
+
+    if (response.statusCode == 200) {
+      return RestaurantDetail.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant detail');
     }
   }
 }
