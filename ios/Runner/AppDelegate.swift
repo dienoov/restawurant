cbf40593e06c39e74a715ece6b60a1168015fc93
dienoov <alvinodien@yahoo.co.id>
2025-04-17 09:46:26 +0700

    chore: add notification and background task setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,32 @@
 import Flutter
 import UIKit
+import workmanager
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+      UIApplication.shared.cancelAllLocalNotifications()
+      UserDefaults.standard.set(true, forKey: "Notification")
+    }
+
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
+
+    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "com.restawurant.restawurant", frequency: NSNumber(value: 16 * 60))
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
