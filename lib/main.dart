9c781449eb3d2becfad90e6a7d39e9a0fafc9983
dienoov <alvinodien@yahoo.co.id>
2025-04-17 09:49:49 +0700

    feat: implement daily notifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:restawurant/apis/restaurants.dart';
 import 'package:restawurant/databases/bookmark.dart';
 import 'package:restawurant/providers/bookmarks.dart';
+import 'package:restawurant/providers/notifications.dart';
 import 'package:restawurant/providers/restaurant.dart';
 import 'package:restawurant/providers/restaurants.dart';
 import 'package:restawurant/providers/search.dart';
@@ -12,9 +13,16 @@ import 'package:restawurant/screens/home.dart';
 import 'package:restawurant/screens/restaurant.dart';
 import 'package:restawurant/screens/search.dart';
 import 'package:restawurant/screens/settings.dart';
+import 'package:restawurant/services/notifications.dart';
+import 'package:restawurant/services/workmanager.dart';
 import 'package:restawurant/styles/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await WorkmanagerService().init();
+  await NotificationsService().init();
+
   runApp(
     MultiProvider(
       providers: [
@@ -31,6 +39,7 @@ void main() {
           create: (context) => SearchProvider(RestaurantsApi()),
         ),
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => NotificationsProvider()),
       ],
       child: const Restawurant(),
     ),
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restawurant/providers/notifications.dart';
 import 'package:restawurant/providers/theme.dart';
+import 'package:restawurant/services/notifications.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,6 +12,31 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  Future<void> onChanged() async {
+    final NotificationsProvider notificationsProvider =
+        Provider.of<NotificationsProvider>(context, listen: false);
+
+    final bool isGranted =
+        await NotificationsService().requestPermissions() ?? false;
+
+    if (isGranted) {
+      notificationsProvider.toggle();
+    } else if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Theme.of(context).colorScheme.onError,
+          content: Text(
+            'Notifications permission denied',
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: Theme.of(context).colorScheme.error,
+            ),
+          ),
+          duration: const Duration(seconds: 1),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -51,6 +78,20 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     ThemeMode.dark,
                 onChanged: (value) {
                   Provider.of<ThemeProvider>(context, listen: false).toggle();
+                },
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Daily Notifications',
+                style: Theme.of(
+                  context,
+                ).textTheme.labelLarge?.copyWith(fontWeight: FontWeight.w600),
+              ),
+              trailing: Switch(
+                value: Provider.of<NotificationsProvider>(context).isEnabled,
+                onChanged: (value) async {
+                  await onChanged();
                 },
               ),
             ),
